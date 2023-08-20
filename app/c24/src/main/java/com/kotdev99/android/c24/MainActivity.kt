a08747d223da45cd3dad7781e0170ff52f090025
KotDev99 <KotDev99@gmail.com>
2023-08-20 10:35:31 +0900

    [MERGE] : ✅Touch Event 회차

---
+++ app/c24/src/main/java/com/kotdev99/android/c24/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.kotdev99.android.c24
+
+import android.os.Bundle
+import android.view.MotionEvent
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+	lateinit var resultView: TextView
+
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_main)
+
+		resultView = findViewById(R.id.txt_resultView)
+	}
+
+	override fun onTouchEvent(event: MotionEvent?): Boolean {
+		var eventType = ""
+		when (event?.action) {
+			MotionEvent.ACTION_DOWN -> eventType = "DOWN EVENT"
+			MotionEvent.ACTION_UP -> eventType = "UP EVENT"
+			MotionEvent.ACTION_MOVE -> eventType = "MOVE EVENT"
+		}
+		resultView.text = "$eventType : x - ${event?.x}, y - ${event?.y}"
+		return super.onTouchEvent(event)
+	}
+}
