87343a1e86772748b737a102f046d6cc982efb17
KotDev99 <KotDev99@gmail.com>
2023-08-24 15:43:17 +0900

    [MERGE] : ✅RecyclerView 회차

---
+++ app/c92/src/main/java/com/kotdev99/android/c92/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.kotdev99.android.c92
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.kotdev99.android.c92.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		val binding = ActivityMainBinding.inflate(layoutInflater)
+		setContentView(binding.root)
+
+		val datas = mutableListOf<String>()
+		for (i in 1..10) {
+			datas.add("item $i")
+		}
+
+		binding.recyclerView.layoutManager = LinearLayoutManager(this)
+		binding.recyclerView.adapter = MyAdapter(datas)
+		binding.recyclerView.addItemDecoration(
+			DividerItemDecoration(
+				this,
+				LinearLayoutManager.VERTICAL
+			)
+		)
+	}
+}
---
+++ app/c92/src/main/java/com/kotdev99/android/c92/MyAdapter.kt
@@ -0,0 +1,27 @@
+package com.kotdev99.android.c92
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.kotdev99.android.c92.databinding.ItemMainBinding
+
+class MyViewHolder(val binding: ItemMainBinding) : RecyclerView.ViewHolder(binding.root)
+
+class MyAdapter(val datas: List<String>) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+	override fun getItemCount(): Int {
+		return datas.size
+	}
+
+	override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+		return MyViewHolder(
+			ItemMainBinding.inflate(
+				LayoutInflater.from(parent.context), parent, false
+			)
+		)
+	}
+
+	override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+		val binding = (holder as MyViewHolder).binding
+		binding.itemData.text = datas[position]
+	}
+}
