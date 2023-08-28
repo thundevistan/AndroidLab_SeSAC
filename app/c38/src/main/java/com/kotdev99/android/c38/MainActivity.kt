3967302451666e490250a820425c53ecec1d5346
KotDev99 <KotDev99@gmail.com>
2023-08-28 22:11:29 +0900

    [MERGE] : ✅MenuInflater 회차

---
+++ app/c38/src/main/java/com/kotdev99/android/c38/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.kotdev99.android.c38
+
+import android.os.Bundle
+import android.view.Menu
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_main)
+	}
+
+	override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+		menuInflater.inflate(R.menu.menu_main, menu)
+		return super.onCreateOptionsMenu(menu)
+	}
+}
