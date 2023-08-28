020bff99c4bea1b5dfd38a899e57479141c45259
KotDev99 <KotDev99@gmail.com>
2023-08-28 20:31:25 +0900

    [MERGE] : ✅Concepts of ActionBar 회차

---
+++ app/c36/src/main/java/com/kotdev99/android/c36/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.kotdev99.android.c36
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_main)
+	}
+}
